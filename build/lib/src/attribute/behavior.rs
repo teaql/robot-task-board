11769d84cf0e8462ebe6193e728b423878f94139
Philip Z <hello@teaql.io>
2026-05-29 23:37:55 +0800

    chore: remove accidentally generated build artifacts

--- build/lib/src/attribute/behavior.rs
+++
@@ -1,6 +0,0 @@
-use teaql_runtime::RepositoryBehavior;
-
-#[derive(Clone, Debug, Default)]
-pub struct AttributeBehavior;
-
-impl RepositoryBehavior for AttributeBehavior {}
--- build/lib/src/object/behavior.rs
+++
@@ -1,6 +0,0 @@
-use teaql_runtime::RepositoryBehavior;
-
-#[derive(Clone, Debug, Default)]
-pub struct ObjectBehavior;
-
-impl RepositoryBehavior for ObjectBehavior {}
