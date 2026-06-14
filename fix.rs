049228972cd43a365e53d5922fad4bf8012bc6d8
Philip Z <hello@teaql.io>
2026-06-14 08:44:44 +0800

    chore: clean up unrelated generated files and zips

--- fix.rs
+++
@@ -1,8 +0,0 @@
-use std::fs;
-fn main() {
-    let path = "/home/philip/githome/teaql-code-gen/generator/src/main/resources/generator/stacks/rust/expression/index.stg";
-    let mut content = fs::read_to_string(path).unwrap();
-    
-    // It's messed up because of literal newlines inside panic!("...")
-    // Let's just restore the file to the original and re-apply cleanly.
-}
--- java_out/lib/src/main/java/com/doublechaintech/robotkanbanservice/RobotKanbanServiceUserContext.java
+++
@@ -1,9 +0,0 @@
-package com.doublechaintech.robotkanbanservice;
-
-import io.teaql.data.UserContext;
-public class RobotKanbanServiceUserContext extends UserContext{
-
-
-
-
-}
